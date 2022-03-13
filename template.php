a63925038ec403dce61fcb102e83fec0185597d8
TeemoCell <11705513+TeemoCell@users.noreply.github.com>
2022-03-13 19:51:23 +0100

    Update template.php

--- template.php
+++ template.php
@@ -127,7 +127,6 @@ if (!class_exists('Noir_Panel_Template')) {
 						');
 
                         $this->addJSFiles(array(
-                            // self::$PUBLIC_PATH . '/assets/plugins/ckeditor/ckeditor.js' => array(),
                             self::$PUBLIC_PATH . '/assets/plugins/ckeditor5/build/ckeditor.js' => array(),
                             self::$PUBLIC_PATH . '/assets/js/pages/debugging_and_maintenance.js' => array()
                         ));
